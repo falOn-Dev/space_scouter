537a80bd3add140f3f4d7131a72460c3ebb73110
Falon <47285315+Tatiman-Dev@users.noreply.github.com>
2023-06-07 21:16:32 -0400

    updated main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import score_handler as sh
 
 values = j.get_value("Auto", "Cubes", "top")
 
-perfect_game = [3,0,0,0,0,False,False,False]
+perfect_game = [3,3,6,6,9,True,True,True]
 
 score_values = [0.15, 0.1, True, False, 0.01, True, True, False]
 
