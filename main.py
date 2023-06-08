dde9b38a3e538c75feaca6c8d633db9327897af9
Falon <47285315+Tatiman-Dev@users.noreply.github.com>
2023-06-07 21:17:34 -0400

    added whitespaces

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import score_handler as sh
 
 values = j.get_value("Auto", "Cubes", "top")
 
-perfect_game = [3,3,6,6,9,True,True,True]
+perfect_game = [3, 3, 6, 6, 9, True, True, True]
 
 score_values = [0.15, 0.1, True, False, 0.01, True, True, False]
 
 if __name__ == '__main__':
     weights = j.get_weights("Auto")
-    final_score = sh.calculate_scores(perfect_game,weights)
+    final_score = sh.calculate_scores(perfect_game, weights)
     print(final_score)
-
-
