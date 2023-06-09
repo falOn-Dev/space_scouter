d1ac84d52829cafa31915b996342160837f08d15
Falon <47285315+Tatiman-Dev@users.noreply.github.com>
2023-06-09 01:31:39 -0400

    Removed link scoring

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ values = j.get_value("Auto", "Cubes", "top")
 
 perfect_auto = [3, 3, 6, 6, 9, True, True, True]
 perfect_tele = [3, 3, 6, 6, 9, 15, True, True, True]
-perfect_endgame = [5, True, True, True, True]
+perfect_endgame = [True, True, True, True]
 
 score_values = [0.15, 0.1, True, False, 0.01, True, True, False]
 
@@ -16,6 +16,7 @@ if __name__ == '__main__':
     print(sh.interpolate_score_placement(410, "teleop"))
     #rh.ranker("teleop")
     #rh.ranker("auto")
+    rh.ranker("endgame")
 
 
 
