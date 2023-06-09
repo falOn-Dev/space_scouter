ce2ceff5cda2a94eae218491aae74f483fbad88e
Falon <47285315+Tatiman-Dev@users.noreply.github.com>
2023-06-08 23:45:36 -0400

    Disallowed duplicate scoring

--- ranking_handler.py
+++ ranking_handler.py
@@ -6,13 +6,14 @@ import pandas as pd
 
 
 def auto_ranker():
-    ranks = []
+    ranks = set()  # Use a set to disallow duplicate values
     for filename in os.listdir('scores'):
         if filename.endswith('.json'):
             data = pd.read_json('scores/' + filename)
             score = data['scores']['auto']
-            ranks.append(score)
-    ranks.sort(reverse=True)
+            ranks.add(score)  # Add score to the set
+
+    ranks = sorted(ranks, reverse=True)  # Sort the set in descending order
     print(ranks)
 
     json_object = {str(100 - index): int(value) for index, value in enumerate(ranks)}
@@ -20,6 +21,7 @@ def auto_ranker():
     # Convert the dictionary to a JSON string
     with open('rankings/auto_rankings.json', 'w') as outfile:
         json.dump(json_object, outfile, indent=4)
+
 
 
 def tele_ranker():
