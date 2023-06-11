17ba5814416c0769769d87f4608f615231d5a063
Falon <47285315+Tatiman-Dev@users.noreply.github.com>
2023-06-11 16:11:30 -0400

    Modified main.py

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ score_values = [0.15, 0.1, True, False, 0.01, True, True, False]
 
 if __name__ == '__main__':
 
-    #app = App()
-    #app.mainloop()
+    app = App()
+    app.mainloop()
 
     # for i in range(50):
-    sh.create_score_file(2537, perfect_auto, perfect_tele, perfect_endgame)
+    #sh.create_score_file(2537, perfect_auto, perfect_tele, perfect_endgame)
     # print(sh.interpolate_score_placement(410, "teleop"))
     # rh.ranker("teleop")
     # rh.ranker("auto")
