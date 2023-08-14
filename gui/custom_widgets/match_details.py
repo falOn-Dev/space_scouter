95b1ddde56d5fa694cbd3e27efc77f35733fb6d6
Falon <47285315+Tatiman-Dev@users.noreply.github.com>
2023-08-14 18:15:08 -0400

    Added match details custom widget

---
+++ gui/custom_widgets/match_details.py
@@ -0,0 +1,8 @@
+import customtkinter as ctk
+
+class MatchDetails(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.configure(border_color="#2FA572", border_width=2, corner_radius=5)
+
